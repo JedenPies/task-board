63debadafc4e0349be6ef25f760c5d48ab9beb26
Patryk Dobrowolski <patryk.dobrowolski@gmail.com>
2026-07-10 15:50:46 +0200

    look and feel

--- task-board-frontend/src/app/components/board-list/board-list.ts
+++ task-board-frontend/src/app/components/board-list/board-list.ts
@@ -1,19 +1,22 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, effect, inject, OnInit, signal, ViewChild } from '@angular/core';
 import { TaskBoardDto } from '../../models/board.model';
 import { TaskBoardService } from '../../services/task-board';
 import { CommonModule } from '@angular/common';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../services/auth';
 import { FormsModule } from '@angular/forms';
+import { LoginModal } from '../login-modal/login-modal';
 
 @Component({
   selector: 'app-board-list',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterLink ],
+  imports: [CommonModule, FormsModule, RouterLink, LoginModal ],
   templateUrl: './board-list.html',
   styleUrl: './board-list.scss',
 })
-export class BoardList implements OnInit {
+export class BoardList {
+
+  @ViewChild(LoginModal) loginModal!: LoginModal;
 
   authService = inject(AuthService);
   taskBoardService = inject(TaskBoardService);
@@ -27,11 +30,11 @@ export class BoardList implements OnInit {
   password = '';
   loginError = signal<string | null>(null);
 
-  ngOnInit(): void {
+  private dataLoader = effect(() => {
     if (this.authService.isLoggedIn()) {
       this.loadBoards();
     }
-  }
+  })
 
   loadBoards() {
     this.taskBoardService.findAllBoards().subscribe(data => {
---
+++ task-board-frontend/src/app/components/login-modal/login-modal.ts
@@ -0,0 +1,36 @@
+import { Component, ElementRef, ViewChild, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth';
+
+@Component({
+  selector: 'app-login-modal',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login-modal.html',
+  styleUrls: ['./login-modal.scss'],
+})
+export class LoginModal {
+  @ViewChild('dialog') dialog!: ElementRef<HTMLDialogElement>;
+  username = '';
+  password = '';
+  authService = inject(AuthService);
+
+  open() {
+    this.dialog.nativeElement.showModal();
+  }
+  close() {
+    this.dialog.nativeElement.close();
+  }
+
+  onLogin(event: Event) {
+    event.preventDefault();
+    this.authService.login(this.username, this.password).subscribe({
+      next: () => {
+        this.username = '';
+        this.password = '';
+        this.close();
+      },
+      error: () => alert('Błąd logowania'),
+    });
+  }
+}
