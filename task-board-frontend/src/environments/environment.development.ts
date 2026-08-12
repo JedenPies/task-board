e7c908e82512f626682d1348c16f862b7257b9f2
Patryk Dobrowolski <patryk.dobrowolski@gmail.com>
2026-08-12 11:27:59 +0200

    delete environment.development.ts

--- task-board-frontend/src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  googleClientId: '414596253914-8cdt79p5tk6biv66pk1h0o21l40gd8uu.apps.googleusercontent.com',
-  githubClientId: 'Ov23li2tJ3I9sGVFdlva',
-};
